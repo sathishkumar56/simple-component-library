5b2feac22e1d8d9d0dde16d8554274ae924aa0d8
krishnaraj.vs <krishnarajvs10@gmail.com>
2023-04-04 18:40:43 +0530

    sa

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import Login from "./lib/TextInput";
+
+import ReactDOM from "react-dom";
+
+ReactDOM.render(React.createElement(
+  "div",
+  null,
+  "Hello world"
+), document.getElementById("root"));
+export { Login };
