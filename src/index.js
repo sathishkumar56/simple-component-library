2174c3053bf11e287939a191e8c7d165fe5a21fb
krishnaraj.vs <krishnarajvs10@gmail.com>
2023-04-04 10:22:16 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+import Login from "./lib/TextInput";
+
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<div>Hello world</div>, document.getElementById("root"));
+export { Login };
