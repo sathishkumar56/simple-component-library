acfa09e3f0ec6c3747c08d0ca0403a1fbd39e2b8
krishnaraj.vs <krishnarajvs10@gmail.com>
2023-04-05 11:51:51 +0530

    sa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 import React from "react";
-import Login from "./lib/TextInput";
-import Carousel1 from "./caurosel/caurosel";
+import {default as Login }from "./lib/TextInput";
+import {default as Carousel1} from "./caurosel/caurosel";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(<div>Hello world</div>, document.getElementById("root"));
