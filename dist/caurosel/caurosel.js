0ef39146f48054ae966d583d24f5a7a16d44fa92
krishnaraj.vs <krishnarajvs10@gmail.com>
2023-04-05 11:52:01 +0530

    sa

---
+++ dist/caurosel/caurosel.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+function Carousel1(props) {
+    return React.createElement(
+        "div",
+        null,
+        React.createElement(
+            Carousel,
+            {
+                infiniteLoop: true,
+                autoPlay: true,
+                showStatus: false,
+                showArrows: true,
+                showThumbs: false,
+                interval: 1500
+            },
+            React.createElement(
+                "div",
+                null,
+                React.createElement("img", { src: props.s1, alt: "image1" })
+            ),
+            React.createElement(
+                "div",
+                null,
+                React.createElement("img", { src: props.s2, alt: "image2" })
+            ),
+            React.createElement(
+                "div",
+                null,
+                React.createElement("img", { src: props.s3, alt: "image3" })
+            ),
+            React.createElement(
+                "div",
+                null,
+                React.createElement("img", { src: props.s4, alt: "image4" })
+            )
+        )
+    );
+}
+export default Carousel1;
---
+++ dist/index.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import { default as Login } from "./lib/TextInput";
+import { default as Carousel1 } from "./caurosel/caurosel";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(React.createElement(
+  "div",
+  null,
+  "Hello world"
+), document.getElementById("root"));
+export { Login, Carousel1 };
